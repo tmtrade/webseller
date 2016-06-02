a6234be007d037553fb863600ae1a2cc28ca4c7f
xuni <xuni@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-06-02 08:50:51 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/webseller@10145 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- Config/app.config.php
+++ Config/app.config.php
@@ -9,7 +9,7 @@ define('SEC', 180);//验证码有效时间
 //define('SITE_URL', 'http://t.chofn.net/');
 //define('TRADE_URL', 'http://tr.chofn.net/');
 //define('GUANJIA_URL', 'http://s.chofn.net/');
-define('USER_CENTRE', 'http://uc2.test.chofn.net/');
+define('USER_CENTRE', 'http://seller.chofn.net/');//出售者平台
 define('CRM_URL', 'http://demo.chofn.com:88/');
 define('CRM_EXPORT','http://crmwin.chofn.net/Api/jsontoexcel.php');
 ?>
