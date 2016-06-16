33d1c548526ada80f2f7e933886b95bdb906a3ea
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-06-16 01:52:29 +0000

    将出售者平台相关移动到seller项目中去
    git-svn-id: http://192.168.0.5/svn/Member/trade/webseller@10364 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- Config/Table/tradenew.table.config.php
+++ Config/Table/tradenew.table.config.php
@@ -18,4 +18,21 @@ $tbl['salehistory'] = array(
 	'dbId'		=> $dbId, 
 	'configFile'=> $configFile,
 );
+$tbl['messege'] = array(
+	'name'		=> $prefix.'messege',
+	'dbId'		=> $dbId,
+	'configFile'=> $configFile,
+);
+
+$tbl['messegeMonitor'] = array(
+	'name'		=> $prefix.'messege_monitor',
+	'dbId'		=> $dbId,
+	'configFile'=> $configFile,
+);
+
+$tbl['messegeUser'] = array(
+	'name'		=> $prefix.'messege_user',
+	'dbId'		=> $dbId,
+	'configFile'=> $configFile,
+);
 ?>
