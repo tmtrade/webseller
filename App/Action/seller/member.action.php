51a73ef98e1b47a8ce440407f7dc2b935689bd2a
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-06-30 03:55:51 +0000

    添加修改手机功能
    git-svn-id: http://192.168.0.5/svn/Member/trade/webseller@10608 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

---
+++ App/Action/seller/member.action.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 用户控制器
+ * Created by PhpStorm.
+ * User: dower
+ * Date: 2016/6/30 0030
+ * Time: 上午 11:12
+ */
+class MemberAction extends AppAction{
+    public function bind(){
+        $this->display();
+    }
+}
