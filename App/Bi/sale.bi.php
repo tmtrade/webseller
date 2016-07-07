3dbbd9a3eed5cf0240a51f2bdbf6364f80ccc0a4
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-07-07 02:59:14 +0000

    git-svn-id: http://192.168.0.5/svn/Member/trade/webseller@10708 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- App/Bi/sale.bi.php
+++ App/Bi/sale.bi.php
@@ -17,17 +17,6 @@ class SaleBi extends Bi
     //添加联系人
     public function addSale($data)
     {
-	    $actionName = 'systemapi';
-	    $funName     = 'addSale';
-//	    $param = array(
-//		'uid'           => '58001',//用户中心ID
-//		'number'        => '100',//商标号
-//		'phone'         => '13551111112',//联系人电话
-//		'contact'       => '标大帅',//联系人名称
-//		'price'         => 44445,//底价
-//		'type'          => 1,//1，出售，2，许可，3，出售+许可
-//		'source'        => 4,//1，用户中心。
-//	    );
 	    $params = array(
 		    'user' => 'api1010',
 		    'sign' => $this->sign($data),
@@ -39,12 +28,6 @@ class SaleBi extends Bi
     //修改联系人价格
     public function updateContactPrice($data)
     {
-	    $actionName = 'systemapi';
-	    $funName     = 'updateContactPrice';
-	    // $param = array(
-	    //     'cid'           => '170151',//联系人信息ID
-	    //     'price'         => 12345,//底价
-	    // );
 	    $params = array(
 		    'user' => 'api1010',
 		    'sign' => $this->sign($data),
@@ -56,12 +39,6 @@ class SaleBi extends Bi
     //取消联系人价格
     public function cancelContact($data)
     {
-	    $actionName = 'systemapi';
-	    $funName     = 'cancelContact';
-	    // $param = array(
-	    //     'uid'           => '5800',
-	    //     'number'         => '100',
-	    // );
 	    $params = array(
 		    'user' => 'api1010',
 		    'sign' => $this->sign($data),
