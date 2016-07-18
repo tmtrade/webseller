0ca9f0ef181890e720e53545fabfdb49b4df016b
dower <dower@8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4>
2016-07-18 10:42:30 +0000

    placeholder
    git-svn-id: http://192.168.0.5/svn/Member/trade/webseller@11176 8c2d0303-d7d2-0546-a2d7-8b6cbec2f4b4

--- Static/1.0/js/table-list.js
+++ Static/1.0/js/table-list.js
@@ -127,37 +127,39 @@ $("input").focus(function(){
 })
 
 //placeholder兼容IE//placeholder兼容IE
-//判断浏览器是否支持placeholder属性
-supportPlaceholder = 'placeholder' in document.createElement('input'),
-    placeholder = function (input) {
-        var text = input.attr('placeholder'),
-            defaultValue = input.defaultValue;
-        var _val = $.trim(input.val());
-        if (!defaultValue && _val == '') {
-            input.val(text).addClass("phcolor");
-        }
-        input.focus(function () {
-            if (input.val() == text) {
-                $(this).val("");
+support_placeholder();
+function support_placeholder(){
+    if(!check_support()){
+        $('input').each(function () {
+            text = $(this).attr("placeholder");
+            if ($(this).attr("type") == "text") {
+                placeholder($(this));
             }
         });
-        input.blur(function () {
-            if (input.val() == "") {
-                $(this).val(text).addClass("phcolor");
-            }
-        });
-//输入的字符不为灰色
-        input.keydown(function () {
-            $(this).removeClass("phcolor");
-        });
-    };
-//当浏览器不支持placeholder属性时，调用placeholder函数
-if (!supportPlaceholder) {
-    $('input').each(function () {
-        text = $(this).attr("placeholder");
-        if ($(this).attr("type") == "text") {
-            placeholder($(this));
+    }
+}
+function  check_support(){
+    return 'placeholder' in document.createElement('input');
+}
+function placeholder(input){
+    var text = input.attr('placeholder'),
+        defaultValue = input.defaultValue;
+    var _val = $.trim(input.val());
+    if (!defaultValue && _val == '') {
+        input.val(text).addClass("phcolor");
+    }
+    input.focus(function () {
+        if (input.val() == text) {
+            $(this).val("");
         }
     });
+    input.blur(function () {
+        if (input.val() == "") {
+            $(this).val(text).addClass("phcolor");
+        }
+    });
+//输入的字符不为灰色
+    input.keydown(function () {
+        $(this).removeClass("phcolor");
+    });
 }
-
